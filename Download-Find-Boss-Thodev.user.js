32511e7f1647b5a3a617583479b47362d17a6b3f
SecurityMod-PrivateT <nguyenquangtho246@gmail.com>
2026-06-09 09:40:07 +0700

    Create Download-Find-Boss-Thodev.user.js

---
+++ Download-Find-Boss-Thodev.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Tho Find Boss - Taming.io (Loader)
+// @namespace    https://taming.io/
+// @version      2.3
+// @match        https://taming.io/*
+// @grant        none
+// @run-at       document-idle
+// @require      https://raw.githubusercontent.com/thodevcode/Menu-Scan-Boss-Taming/refs/heads/main/find-boss.js
+// ==/UserScript==
