a631279c85029a986c28ad5a9030e5b203a19bb1
SecurityMod-PrivateT <nguyenquangtho246@gmail.com>
2026-06-09 09:50:27 +0700

    Create Download-FireWall.user.js

---
+++ Download-FireWall.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         FireWall - By thodevcode
+// @version      forever
+// @match        chrome-extension://*
+// @match        https://taming.io/*
+// @grant        none
+// @run-at       document-idle
+// @require      https://raw.githubusercontent.com/thodevcode/Menu-Scan-Boss-Taming/refs/heads/main/firewall.js
+// ==/UserScript==
